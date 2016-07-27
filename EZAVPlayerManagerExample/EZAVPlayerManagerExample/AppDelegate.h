70b37a5322eee55ae6c57c22f694ca217bc9cbfd
李朋远 <owen@lipengyuandeMacBook-Pro.local>
2016-07-27 15:56:39 +0800

    creat project

---
+++ EZAVPlayerManagerExample/EZAVPlayerManagerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EZAVPlayerManagerExample
+//
+//  Created by 李朋远 on 16/7/27.
+//  Copyright © 2016年 李朋远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EZAVPlayerManagerExample/EZAVPlayerManagerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZAVPlayerManagerExample
+//
+//  Created by 李朋远 on 16/7/27.
+//  Copyright © 2016年 李朋远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
